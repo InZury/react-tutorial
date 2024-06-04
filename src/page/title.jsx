663f2c40735d64462dcda2e6daf24ddfa4addd4f
한인철 <120384393+InZury@users.noreply.github.com>
2024-06-04 22:12:08 +0900

    [Add] 간단 기능 테스트 - > 반드시 수정이 필요

---
+++ src/page/title.jsx
@@ -0,0 +1,7 @@
+export const Title = () => {
+    return(
+        <div className="titleDiv">
+            <h1 className="titleH1"> React Tutorial </h1>
+        </div>
+    );
+};
---
+++ src/route/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Title } from "../page/title";
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <Title />,
+            children:
+            [
+                
+            ]
+        }
+    ]
+);
